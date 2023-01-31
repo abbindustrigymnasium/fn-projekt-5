8d526895d3876b659a2d748c53d576836b79f3e5
YayL <zimon.moudi@abbgymnasiet.se>
2023-01-31 13:57:06 +0100

    Added automatic error handling

--- src/routes/users/list.ts
+++ src/routes/users/list.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 module.exports = {
     subpath: 'list',
     method: 'get',
-    run: async function(request: Request, response: Response) {
+    run: async function(request: Request, response: Response, next: NextFunction) {
         let prisma = request.app.get("prisma");
         const users = await prisma.user.findMany()
         response.json(users);
