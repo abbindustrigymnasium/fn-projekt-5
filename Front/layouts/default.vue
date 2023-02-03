e69783fa8dc042caaefdf26c3153dd47e8e84dd5
Erra <erling.blomberg@abbgymnasiet.se>
2023-02-03 10:23:24 +0100

    erling

---
+++ Front/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h-screen">
+        <NavBar />
+        <div class="h-14"></div>
+        <slot class="min-h-full" />
+        <Footer />
+    </div>
+</template>
+
+<script>
+export default {
+    head () {
+        return {
+            title: "FN",
+        }
+    },
+}
+</script>
--- Front/pages/index.vue
+++ Front/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    oi
+  <div class="bg-slate-800 min-h-screen">
+    <div class="text-teal-400 h-screen">
+      oiii
+    </div>
+    <div class="bg-slate-900 text-white h-screen">
+      oiii
+    </div>
   </div>
 </template>
