4ba8ee4537626eef7413589019028b42102a64fe
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2023-02-06 11:08:16 +0100

    Implomented functions Working system for resistry insuring everything is as it´s suposed to be

--- Front/pages/register.vue
+++ Front/pages/register.vue
@@ -1,3 +1,49 @@
+<script setup>
+import { ref, watch, onMounted } from "vue"
+
+const isPassword = ref(false)
+const type = ref("password")
+const acceptTOS = ref(false)
+
+const UserEmail = ref()
+const Username = ref()
+const Password = ref()
+const PasswordCheck = ref()
+
+watch(isPassword, (val) => {
+	if (val) {
+		type.value = "text"
+	} else {
+		type.value = "password"
+	}
+})
+
+function forgotPassword() {
+	alert("Hint: Your Cats Name!")
+}
+
+function Register() {
+    console.log(Password.value, " ", PasswordCheck.value, " ", UserEmail.value, " ", Username.value)
+    if (!Password.value || !UserEmail.value || !Username.value){
+        alert("Fileds can not be empty!")
+    } else {
+        if (acceptTOS.value == false) {
+            alert("You must accept our Terms Of Service to register")
+        } else {
+            if (Password.value != PasswordCheck.value) {
+                alert("Passwords must match")
+            } else {
+                alert("Passwords match, user "+Username.value+" registered!")
+            }
+        }
+    }
+}
+
+onMounted(() => {
+  console.log(`Script Works!!`)
+})
+</script>
+
 <template>
     <div>
         <div class="z-10 flex justify-center mt-24 h-screen">
@@ -6,40 +52,28 @@
                 <h1 class="text-5xl mt-2 font-serif">Register</h1>
                 </div>
                 <div class="flex justify-center flex-col">
-                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="UserEmail" placeholder="Email ex.Jimny.bob@example.com">
-                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="Username" placeholder="Username ex.JimnyBob77">
-                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" v-model="Password" placeholder="Password" :type="type" id="password"> 
-                    <button type="checkbox" class="focus:outline-none">Remember me</button>
-                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="password" v-model="PasswordCheck" placeholder="Confirm Password">
+                    <input class="px-4 h-8 w-72 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="UserEmail" placeholder="Email ex. Jimny.bob@example.com">
+                    <input class="px-4 h-8 w-72 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="Username" placeholder="Username ex.JimnyBob77">
+                    <div class="flex flex-row">   
+                    <input class="px-4 h-8 w-72 ml-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" v-model="Password" placeholder="Password" :type="type"> 
+                    <input type="checkbox" class="ml-2 mt-4 focus:outline-none" v-model="isPassword">
+                    </div>
+                    <input class="px-4 h-8 w-72 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="password" v-model="PasswordCheck" placeholder="Confirm Password">
                 </div>
                 <div class="flex justify-center">  
                     <input class="mt-4 mr-2 focus:outline-none" type="checkbox" name="TOS" v-model="acceptTOS">
                     <p class="mt-4 text-white text-sm">I have read and agree to the <a class="no-underline hover:underline focus:outline-none focus:underline" href="/TOS">Terms Of Service</a></p>
                 </div>
                 <div class="flex justify-center pt-8 space-x-24">
-                    <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-teal-700 focus:outline-none focus:bg-teal-700" onclick="forgotPassword()">Register</button>
-                    <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-red-500 focus:outline-none focus:bg-red-500" >Cancel</button>
+                    <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-teal-700 focus:outline-none focus:bg-teal-700" @click="Register">Register</button>
+                    <input type="button" class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-red-500 focus:outline-none focus:bg-red-500" href="/" value="Cancel">
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
-import { ref, watch } from "vue";
-const Password = ref(false);
-const type = ref("password");
-watch(Password, (val) => {
-	if (val) {
-		type.value = "text";
-	} else {
-		type.value = "password";
-	}
-});
-function forgotPassword() {
-	alert("I am an alert box!");
-}
-</script>
+
 
 <style>
 body {
