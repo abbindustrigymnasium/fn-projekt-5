7f8b1bcafdf18957d399233bbe5fefec575aca59
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2023-02-06 09:12:33 +0100

    made an attempt at scripting messed around with viewing password with v-model

--- Front/pages/register.vue
+++ Front/pages/register.vue
@@ -8,7 +8,8 @@
                 <div class="flex justify-center flex-col">
                     <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="UserEmail" placeholder="Email ex.Jimny.bob@example.com">
                     <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="text" v-model="Username" placeholder="Username ex.JimnyBob77">
-                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="password" v-model="Password" placeholder="Password">
+                    <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" v-model="Password" placeholder="Password" :type="type" id="password"> 
+                    <button type="checkbox" class="focus:outline-none">Remember me</button>
                     <input class="px-4 h-8 mx-16 mt-4 bg-gray-700 rounded-lg focus:bg-gray-500 focus:outline-none" type="password" v-model="PasswordCheck" placeholder="Confirm Password">
                 </div>
                 <div class="flex justify-center">  
@@ -16,13 +17,29 @@
                     <p class="mt-4 text-white text-sm">I have read and agree to the <a class="no-underline hover:underline focus:outline-none focus:underline" href="/TOS">Terms Of Service</a></p>
                 </div>
                 <div class="flex justify-center pt-8 space-x-24">
-                    <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-teal-700 focus:outline-none focus:bg-teal-700" >Register</button>
+                    <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-teal-700 focus:outline-none focus:bg-teal-700" onclick="forgotPassword()">Register</button>
                     <button class="bg-teal-500 h-16 w-24 text-2xl rounded-xl hover:bg-red-500 focus:outline-none focus:bg-red-500" >Cancel</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref, watch } from "vue";
+const Password = ref(false);
+const type = ref("password");
+watch(Password, (val) => {
+	if (val) {
+		type.value = "text";
+	} else {
+		type.value = "password";
+	}
+});
+function forgotPassword() {
+	alert("I am an alert box!");
+}
+</script>
 
 <style>
 body {
