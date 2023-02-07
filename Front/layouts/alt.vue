ef661f0ed5d790afd7fb3f12e2d6cc99f70a4360
Erra <erling.blomberg@abbgymnasiet.se>
2023-02-07 12:44:27 +0100

    temp disabled article rendering

---
+++ Front/layouts/alt.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-screen">
+        <p class="text-2xl">ALT</p>
+        <NavBar />
+        <div class="h-14"></div>
+        <slot class="min-h-full" />
+        <Footer />
+    </div>
+</template>
+
+<script>
+export default {
+    head () {
+        return {
+            title: "FN",
+        }
+    },
+}
+</script>
