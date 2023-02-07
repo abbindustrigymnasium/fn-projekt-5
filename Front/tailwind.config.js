71f36d4a9bf3e75e6cc6e5b82e57734ec9c4d149
21ABBMax <92145624+21ABBMax@users.noreply.github.com>
2023-02-07 12:13:23 +0100

    jes

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./nuxt.config.{js,ts}",
+		"./app.vue",
+		"./pages/**/*.{html,js}",
+		"./components/**/*.{html,js}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
