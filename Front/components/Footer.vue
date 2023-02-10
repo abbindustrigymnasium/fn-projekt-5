5ca0d56afb9c759f5faead681ef970643cf33c06
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2023-02-10 08:48:50 +0100

    Modified Footer to be better fitted to the website

--- Front/components/Footer.vue
+++ Front/components/Footer.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="bg-gradient-to-r from-teal-500 to-teal-400 h-72">
-        la
-    </div>
+	<div class="bg-gradient-to-r from-teal-400 to-teal-600 w-full flex flex-row">
+		<p class="py-4 px-4">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien in
+			bibendum dictum, enim enim pellentesque tellus, at malesuada elit diam non turpis.
+			In hac habitasse platea dictumst. Ut ac ipsum gravida, gravida libero id, tincidunt
+			velit. Nam euismod suscipit varius.
+		</p>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
